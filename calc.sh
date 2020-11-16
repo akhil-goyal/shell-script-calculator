ab9ce5cb2ce65d36018de4af547058f20434db7a
Akhil Goyal <akhilgoyal@MacBook-Pro.phub.net.cable.rogers.com>
2020-11-16 11:37:50 -0500

    Added User Menu, switch case & function for addition of two numbers.

--- calc.sh
+++ calc.sh
@@ -17,3 +17,44 @@ fi
 
 echo $greetingMessage
 # --------------------------------------------------------------------------------
+
+
+# ------------------------------- FUNCTIONS --------------------------------------
+function additionFunction(){
+    echo "The sum of $firstInput and $secondInput is :"
+    expr $firstInput + $secondInput
+}
+# --------------------------------------------------------------------------------
+
+
+# -------------------------- CONDITIONING & LOOPS --------------------------------
+while true; do
+
+# User Menu : It will re-display until the user exits.
+
+  echo "Please choose an operation from the list below :"
+  echo "1. (A)ddition"
+  echo "2. (S)ubtraction"
+  echo "3. (M)ultiplication"
+  echo "4. (D)ivision"
+  echo "5. (E)xit Calculator"
+
+# Capturing the User Input
+
+  read userInput
+
+# Using switch cases to check for user input & invoke the appropriate function.
+
+  case $userInput in
+      "A" | "a" | "1" | "ADD" | "add" | "Add")
+        echo "Enter the first number : "
+        read firstInput
+        echo "Enter the second number : "
+        read secondInput
+
+        # Calling the function additionFunction & setting firstInput & secondInput as parameters.
+        additionFunction $firstInput $secondInput
+      ;;
+  esac
+done
+# --------------------------------------------------------------------------------
